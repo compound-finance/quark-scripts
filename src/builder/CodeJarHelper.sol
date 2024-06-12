9de235f7798c7dc29206cd7a618db29722b12e1f
kevincheng96 <kevincheng96@hotmail.com>
2024-06-12 11:55:27 -0700

    Use actual CodeJar address

--- src/builder/CodeJarHelper.sol
+++ src/builder/CodeJarHelper.sol
@@ -2,21 +2,14 @@
 pragma solidity ^0.8.23;
 
 library CodeJarHelper {
-    function knownCodeJar(uint256 chainId) internal pure returns (address) {
-        if (chainId == 1) {
-            return address(0xff); // FIXME
-        } else if (chainId == 8453) {
-            return address(0xfff); // FIXME
-        } else {
-            revert(); // FIXME
-        }
-    }
+    /* ===== Constants ===== */
+
+    /// @notice The address for CodeJar on all chains
+    address constant CODE_JAR_ADDRESS = 0x2b68764bCfE9fCD8d5a30a281F141f69b69Ae3C8;
 
     function getCodeAddress(uint256 chainId, bytes memory code) public pure returns (address) {
         return address(
-            uint160(
-                uint256(keccak256(abi.encodePacked(bytes1(0xff), knownCodeJar(chainId), uint256(0), keccak256(code))))
-            )
+            uint160(uint256(keccak256(abi.encodePacked(bytes1(0xff), CODE_JAR_ADDRESS, uint256(0), keccak256(code)))))
         );
     }
 }
