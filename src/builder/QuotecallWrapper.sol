da9a9c60a4aed6f7ea7c4fe8647175400fe4b576
Hans Wang <hans@compound.finance>
2024-06-12 12:11:07 -0700

    quotecall wrapper

---
+++ src/builder/QuotecallWrapper.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity ^0.8.23;
+
+import {CodeJarHelper} from "./CodeJarHelper.sol";
+import {IQuarkWallet} from "quark-core/src/interfaces/IQuarkWallet.sol";
+import {Quotecall} from "../Quotecall.sol";
+
+// Helper library to wrap a QuarkOperation from Actions.sol for a Paycall
+library QuotecallWrapper {
+    function wrap(IQuarkWallet.QuarkOperation memory operation, uint256 chainId, uint256 quotedAmount)
+        internal
+        pure
+        returns (IQuarkWallet.QuarkOperation memory)
+    {
+        bytes[] memory scriptSources = new bytes[](1);
+        scriptSources[0] = type(Quotecall).creationCode;
+
+        return IQuarkWallet.QuarkOperation({
+            nonce: operation.nonce,
+            scriptAddress: CodeJarHelper.getCodeAddress(chainId, type(Quotecall).creationCode),
+            scriptCalldata: abi.encodeWithSelector(
+                Quotecall.run.selector, operation.scriptAddress, operation.scriptCalldata, quotedAmount
+                ),
+            scriptSources: scriptSources,
+            expiry: operation.expiry
+        });
+    }
+}
