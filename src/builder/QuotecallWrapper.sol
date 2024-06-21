cea4fedfa8fb0471506adf9f8adb68410cbe104f
Hans Wang <2709448+cwang25@users.noreply.github.com>
2024-06-21 13:01:02 -0700

    Quotecall wrapper (#14)
    Same with paycall, but it's quotecall wrapper

---
+++ src/builder/QuotecallWrapper.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity ^0.8.23;
+
+import {CodeJarHelper} from "./CodeJarHelper.sol";
+import {IQuarkWallet} from "quark-core/src/interfaces/IQuarkWallet.sol";
+import {Quotecall} from "../Quotecall.sol";
+import {PaymentInfo} from "./PaymentInfo.sol";
+
+// Helper library to wrap a QuarkOperation from Actions.sol for a Paycall
+library QuotecallWrapper {
+    function wrap(
+        IQuarkWallet.QuarkOperation memory operation,
+        uint256 chainId,
+        string memory paymentTokenSymbol,
+        uint256 quotedAmount
+    ) internal pure returns (IQuarkWallet.QuarkOperation memory) {
+        PaymentInfo.PaymentToken memory paymentToken = PaymentInfo.knownToken(paymentTokenSymbol, chainId);
+        bytes memory quotecallSource =
+            abi.encodePacked(type(Quotecall).creationCode, abi.encode(paymentToken.priceFeed, paymentToken.token));
+        bytes[] memory scriptSources = new bytes[](operation.scriptSources.length + 1);
+        for (uint256 i = 0; i < operation.scriptSources.length; i++) {
+            scriptSources[i] = operation.scriptSources[i];
+        }
+
+        scriptSources[operation.scriptSources.length] = quotecallSource;
+
+        return IQuarkWallet.QuarkOperation({
+            nonce: operation.nonce,
+            scriptAddress: CodeJarHelper.getCodeAddress(quotecallSource),
+            scriptCalldata: abi.encodeWithSelector(
+                Quotecall.run.selector, operation.scriptAddress, operation.scriptCalldata, quotedAmount
+                ),
+            scriptSources: scriptSources,
+            expiry: operation.expiry
+        });
+    }
+}
