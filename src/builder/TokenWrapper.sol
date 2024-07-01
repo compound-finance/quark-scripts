d7ab425903425dd9819b8f2e6c126c8844db2870
Hans Wang <hans@compound.finance>
2024-07-01 12:44:38 -0700

    add addresses to wrapper

--- src/builder/TokenWrapper.sol
+++ src/builder/TokenWrapper.sol
@@ -14,18 +14,17 @@ library TokenWrapper {
 
     function knownWrappedTokenPairs() internal pure returns (KnownWrappedTokenPair[] memory) {
         KnownWrappedTokenPair[] memory pairs = new KnownWrappedTokenPair[](4);
-        pairs[0] = KnownWrappedTokenPair({chainId: 1, wrapper: 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984, underlyingSymbol: "ETH", wrappedSymbol: "WETH"});
-        pairs[1] = KnownWrappedTokenPair({chainId: 8453, wrapper: 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984, underlyingSymbol: "ETH", wrappedSymbol: "WETH"});
-        pairs[2] = KnownWrappedTokenPair({chainId: 1, wrapper: 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984, underlyingSymbol: "ETH", wrappedSymbol: "WETH"});
-        pairs[3] = KnownWrappedTokenPair({chainId: 8453, wrapper: 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984, underlyingSymbol: "ETH", wrappedSymbol: "WETH"});
+        pairs[0] = KnownWrappedTokenPair({chainId: 1, wrapper: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, underlyingSymbol: "ETH", wrappedSymbol: "WETH"});
+        pairs[1] = KnownWrappedTokenPair({chainId: 8453, wrapper: 0x4200000000000000000000000000000000000006, underlyingSymbol: "ETH", wrappedSymbol: "WETH"});
+        pairs[2] = KnownWrappedTokenPair({chainId: 1, wrapper: 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0, underlyingSymbol: "stETH", wrappedSymbol: "wstETH"});
         return pairs;
     }
 
-    function hasWrappedVersion(string memory tokenSymbol) internal pure returns (bool) {
+    function hasWrappedVersion(uint256 chainId, string memory tokenSymbol) internal pure returns (bool) {
         return Strings.stringEqIgnoreCase(tokenSymbol, "USDC");
     }
 
-    function hasUnwrappedVersion(string memory tokenSymbol) internal pure returns (bool) {
+    function hasUnwrappedVersion(uint256 chainId, string memory tokenSymbol) internal pure returns (bool) {
         return Strings.stringEqIgnoreCase(tokenSymbol, "WUSDC");
     }
 
