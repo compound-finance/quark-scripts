a3fbe158716e070ec4e402616a3aeb67c427d2b9
Hans Wang <hans@legend.xyz>
2024-09-10 14:16:34 -0700

    Revert "the payment test helper function is mistakenly using lower-case usdc ... lol"
    This reverts commit b291e6780ad48a0f268884da3ff98e0a44681a1d.

--- test/builder/lib/QuarkBuilderTest.sol
+++ test/builder/lib/QuarkBuilderTest.sol
@@ -62,7 +62,7 @@ contract QuarkBuilderTest {
         pure
         returns (PaymentInfo.Payment memory)
     {
-        return PaymentInfo.Payment({isToken: true, currency: "USDC", maxCosts: maxCosts});
+        return PaymentInfo.Payment({isToken: true, currency: "usdc", maxCosts: maxCosts});
     }
 
     function paymentUsd_() internal pure returns (PaymentInfo.Payment memory) {
@@ -74,7 +74,7 @@ contract QuarkBuilderTest {
         pure
         returns (PaymentInfo.Payment memory)
     {
-        return PaymentInfo.Payment({isToken: false, currency: "USD", maxCosts: maxCosts});
+        return PaymentInfo.Payment({isToken: false, currency: "usd", maxCosts: maxCosts});
     }
 
     // TODO: refactor
