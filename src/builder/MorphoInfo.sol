d6e3939f808e5ae61d0217fd4a4d0498828f881f
banky <banky@compound.finance>
2024-10-28 17:14:23 -0400

    Add morpho testnet vaults

--- src/builder/MorphoInfo.sol
+++ src/builder/MorphoInfo.sol
@@ -11,7 +11,12 @@ library MorphoInfo {
 
     // Note: Current Morpho has same address across mainnet and base
     function getMorphoAddress(uint256 chainId) internal pure returns (address) {
-        if (chainId == 1 || chainId == 8453 || chainId == 11155111 || chainId == 84532) {
+        if (
+            chainId == 1 ||
+            chainId == 8453 ||
+            chainId == 11155111 ||
+            chainId == 84532
+        ) {
             return 0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb;
         } else {
             revert UnsupportedChainId();
@@ -26,13 +31,21 @@ library MorphoInfo {
         string collateralAssetSymbol;
     }
 
-    function getKnownMorphoMarketsParams() internal pure returns (HashMap.Map memory) {
+    function getKnownMorphoMarketsParams()
+        internal
+        pure
+        returns (HashMap.Map memory)
+    {
         HashMap.Map memory knownMarkets = HashMap.newMap();
         // === Mainnet morpho markets ===
         // WBTC collateral markets
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 1, borrowAssetSymbol: "USDC", collateralAssetSymbol: "WBTC"}),
+            MorphoMarketKey({
+                chainId: 1,
+                borrowAssetSymbol: "USDC",
+                collateralAssetSymbol: "WBTC"
+            }),
             MarketParams({
                 loanToken: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48,
                 collateralToken: 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599,
@@ -43,7 +56,11 @@ library MorphoInfo {
         );
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 1, borrowAssetSymbol: "USDT", collateralAssetSymbol: "WBTC"}),
+            MorphoMarketKey({
+                chainId: 1,
+                borrowAssetSymbol: "USDT",
+                collateralAssetSymbol: "WBTC"
+            }),
             MarketParams({
                 loanToken: 0xdAC17F958D2ee523a2206206994597C13D831ec7,
                 collateralToken: 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599,
@@ -54,7 +71,11 @@ library MorphoInfo {
         );
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 1, borrowAssetSymbol: "WETH", collateralAssetSymbol: "WBTC"}),
+            MorphoMarketKey({
+                chainId: 1,
+                borrowAssetSymbol: "WETH",
+                collateralAssetSymbol: "WBTC"
+            }),
             MarketParams({
                 loanToken: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2,
                 collateralToken: 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599,
@@ -65,7 +86,11 @@ library MorphoInfo {
         );
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 1, borrowAssetSymbol: "PYUSD", collateralAssetSymbol: "WBTC"}),
+            MorphoMarketKey({
+                chainId: 1,
+                borrowAssetSymbol: "PYUSD",
+                collateralAssetSymbol: "WBTC"
+            }),
             MarketParams({
                 loanToken: 0x6c3ea9036406852006290770BEdFcAbA0e23A0e8,
                 collateralToken: 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599,
@@ -76,7 +101,11 @@ library MorphoInfo {
         );
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 1, borrowAssetSymbol: "eUSD", collateralAssetSymbol: "WBTC"}),
+            MorphoMarketKey({
+                chainId: 1,
+                borrowAssetSymbol: "eUSD",
+                collateralAssetSymbol: "WBTC"
+            }),
             MarketParams({
                 loanToken: 0xA0d69E286B938e21CBf7E51D71F6A4c8918f482F,
                 collateralToken: 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599,
@@ -87,7 +116,11 @@ library MorphoInfo {
         );
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 1, borrowAssetSymbol: "USDA", collateralAssetSymbol: "WBTC"}),
+            MorphoMarketKey({
+                chainId: 1,
+                borrowAssetSymbol: "USDA",
+                collateralAssetSymbol: "WBTC"
+            }),
             MarketParams({
                 loanToken: 0x0000206329b97DB379d5E1Bf586BbDB969C63274,
                 collateralToken: 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599,
@@ -99,7 +132,11 @@ library MorphoInfo {
         // wstETH collateral markets
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 1, borrowAssetSymbol: "WETH", collateralAssetSymbol: "wstETH"}),
+            MorphoMarketKey({
+                chainId: 1,
+                borrowAssetSymbol: "WETH",
+                collateralAssetSymbol: "wstETH"
+            }),
             MarketParams({
                 loanToken: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2,
                 collateralToken: 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0,
@@ -110,7 +147,11 @@ library MorphoInfo {
         );
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 1, borrowAssetSymbol: "USDC", collateralAssetSymbol: "wstETH"}),
+            MorphoMarketKey({
+                chainId: 1,
+                borrowAssetSymbol: "USDC",
+                collateralAssetSymbol: "wstETH"
+            }),
             MarketParams({
                 loanToken: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48,
                 collateralToken: 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0,
@@ -121,7 +162,11 @@ library MorphoInfo {
         );
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 1, borrowAssetSymbol: "USDC", collateralAssetSymbol: "wstETH"}),
+            MorphoMarketKey({
+                chainId: 1,
+                borrowAssetSymbol: "USDC",
+                collateralAssetSymbol: "wstETH"
+            }),
             MarketParams({
                 loanToken: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48,
                 collateralToken: 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0,
@@ -132,7 +177,11 @@ library MorphoInfo {
         );
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 1, borrowAssetSymbol: "USDT", collateralAssetSymbol: "wstETH"}),
+            MorphoMarketKey({
+                chainId: 1,
+                borrowAssetSymbol: "USDT",
+                collateralAssetSymbol: "wstETH"
+            }),
             MarketParams({
                 loanToken: 0xdAC17F958D2ee523a2206206994597C13D831ec7,
                 collateralToken: 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0,
@@ -143,7 +192,11 @@ library MorphoInfo {
         );
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 1, borrowAssetSymbol: "eUSD", collateralAssetSymbol: "wstETH"}),
+            MorphoMarketKey({
+                chainId: 1,
+                borrowAssetSymbol: "eUSD",
+                collateralAssetSymbol: "wstETH"
+            }),
             MarketParams({
                 loanToken: 0xA0d69E286B938e21CBf7E51D71F6A4c8918f482F,
                 collateralToken: 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0,
@@ -154,7 +207,11 @@ library MorphoInfo {
         );
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 1, borrowAssetSymbol: "PYUSD", collateralAssetSymbol: "wstETH"}),
+            MorphoMarketKey({
+                chainId: 1,
+                borrowAssetSymbol: "PYUSD",
+                collateralAssetSymbol: "wstETH"
+            }),
             MarketParams({
                 loanToken: 0x6c3ea9036406852006290770BEdFcAbA0e23A0e8,
                 collateralToken: 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0,
@@ -166,7 +223,11 @@ library MorphoInfo {
         // weETH collateral markets
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 1, borrowAssetSymbol: "WETH", collateralAssetSymbol: "weETH"}),
+            MorphoMarketKey({
+                chainId: 1,
+                borrowAssetSymbol: "WETH",
+                collateralAssetSymbol: "weETH"
+            }),
             MarketParams({
                 loanToken: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2,
                 collateralToken: 0xCd5fE23C85820F7B72D0926FC9b05b43E359b7ee,
@@ -178,7 +239,11 @@ library MorphoInfo {
         // MKR collateral markets
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 1, borrowAssetSymbol: "USDC", collateralAssetSymbol: "MKR"}),
+            MorphoMarketKey({
+                chainId: 1,
+                borrowAssetSymbol: "USDC",
+                collateralAssetSymbol: "MKR"
+            }),
             MarketParams({
                 loanToken: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48,
                 collateralToken: 0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2,
@@ -190,7 +255,11 @@ library MorphoInfo {
         // USDe collateral markets
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 1, borrowAssetSymbol: "DAI", collateralAssetSymbol: "USDe"}),
+            MorphoMarketKey({
+                chainId: 1,
+                borrowAssetSymbol: "DAI",
+                collateralAssetSymbol: "USDe"
+            }),
             MarketParams({
                 loanToken: 0x6B175474E89094C44Da98b954EedeAC495271d0F,
                 collateralToken: 0x4c9EDD5852cd905f086C759E8383e09bff1E68B3,
@@ -202,7 +271,11 @@ library MorphoInfo {
         // sUSDe collateral markets
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 1, borrowAssetSymbol: "DAI", collateralAssetSymbol: "sUSDe"}),
+            MorphoMarketKey({
+                chainId: 1,
+                borrowAssetSymbol: "DAI",
+                collateralAssetSymbol: "sUSDe"
+            }),
             MarketParams({
                 loanToken: 0x6B175474E89094C44Da98b954EedeAC495271d0F,
                 collateralToken: 0x9D39A5DE30e57443BfF2A8307A4256c8797A3497,
@@ -215,7 +288,11 @@ library MorphoInfo {
         // WETH collateral markets
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 8453, borrowAssetSymbol: "USDC", collateralAssetSymbol: "WETH"}),
+            MorphoMarketKey({
+                chainId: 8453,
+                borrowAssetSymbol: "USDC",
+                collateralAssetSymbol: "WETH"
+            }),
             MarketParams({
                 loanToken: 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913,
                 collateralToken: 0x4200000000000000000000000000000000000006,
@@ -227,7 +304,11 @@ library MorphoInfo {
         // wstETH collateral markets
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 8453, borrowAssetSymbol: "WETH", collateralAssetSymbol: "wstETH"}),
+            MorphoMarketKey({
+                chainId: 8453,
+                borrowAssetSymbol: "WETH",
+                collateralAssetSymbol: "wstETH"
+            }),
             MarketParams({
                 loanToken: 0x4200000000000000000000000000000000000006,
                 collateralToken: 0xc1CBa3fCea344f92D9239c08C0568f6F2F0ee452,
@@ -239,7 +320,11 @@ library MorphoInfo {
         // cbETH collateral markets
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 8453, borrowAssetSymbol: "USDC", collateralAssetSymbol: "cbETH"}),
+            MorphoMarketKey({
+                chainId: 8453,
+                borrowAssetSymbol: "USDC",
+                collateralAssetSymbol: "cbETH"
+            }),
             MarketParams({
                 loanToken: 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913,
                 collateralToken: 0x2Ae3F1Ec7F1F5012CFEab0185bfc7aa3cf0DEc22,
@@ -250,7 +335,11 @@ library MorphoInfo {
         );
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 8453, borrowAssetSymbol: "WETH", collateralAssetSymbol: "cbETH"}),
+            MorphoMarketKey({
+                chainId: 8453,
+                borrowAssetSymbol: "WETH",
+                collateralAssetSymbol: "cbETH"
+            }),
             MarketParams({
                 loanToken: 0x4200000000000000000000000000000000000006,
                 collateralToken: 0x2Ae3F1Ec7F1F5012CFEab0185bfc7aa3cf0DEc22,
@@ -261,7 +350,11 @@ library MorphoInfo {
         );
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 8453, borrowAssetSymbol: "eUSD", collateralAssetSymbol: "cbETH"}),
+            MorphoMarketKey({
+                chainId: 8453,
+                borrowAssetSymbol: "eUSD",
+                collateralAssetSymbol: "cbETH"
+            }),
             MarketParams({
                 loanToken: 0xCfA3Ef56d303AE4fAabA0592388F19d7C3399FB4,
                 collateralToken: 0x2Ae3F1Ec7F1F5012CFEab0185bfc7aa3cf0DEc22,
@@ -273,7 +366,11 @@ library MorphoInfo {
         // ezETH collateral markets
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 8453, borrowAssetSymbol: "WETH", collateralAssetSymbol: "ezETH"}),
+            MorphoMarketKey({
+                chainId: 8453,
+                borrowAssetSymbol: "WETH",
+                collateralAssetSymbol: "ezETH"
+            }),
             MarketParams({
                 loanToken: 0x4200000000000000000000000000000000000006,
                 collateralToken: 0x2416092f143378750bb29b79eD961ab195CcEea5,
@@ -289,7 +386,11 @@ library MorphoInfo {
         // === Base Sepolia testnet morpho markets ===
         addMarketParams(
             knownMarkets,
-            MorphoMarketKey({chainId: 84532, borrowAssetSymbol: "USDC", collateralAssetSymbol: "WETH"}),
+            MorphoMarketKey({
+                chainId: 84532,
+                borrowAssetSymbol: "USDC",
+                collateralAssetSymbol: "WETH"
+            }),
             MarketParams({
                 loanToken: 0x036CbD53842c5426634e7929541eC2318f3dCF7e,
                 collateralToken: 0x4200000000000000000000000000000000000006,
@@ -302,20 +403,21 @@ library MorphoInfo {
         return knownMarkets;
     }
 
-    function getMarketParams(uint256 chainId, string memory collateralAssetSymbol, string memory borrowAssetSymbol)
-        internal
-        pure
-        returns (MarketParams memory)
-    {
+    function getMarketParams(
+        uint256 chainId,
+        string memory collateralAssetSymbol,
+        string memory borrowAssetSymbol
+    ) internal pure returns (MarketParams memory) {
         HashMap.Map memory knownMarkets = getKnownMorphoMarketsParams();
-        return getMarketParams(
-            knownMarkets,
-            MorphoMarketKey({
-                chainId: chainId,
-                borrowAssetSymbol: borrowAssetSymbol,
-                collateralAssetSymbol: collateralAssetSymbol
-            })
-        );
+        return
+            getMarketParams(
+                knownMarkets,
+                MorphoMarketKey({
+                    chainId: chainId,
+                    borrowAssetSymbol: borrowAssetSymbol,
+                    collateralAssetSymbol: collateralAssetSymbol
+                })
+            );
     }
 
     // Morpho vaults
@@ -325,7 +427,11 @@ library MorphoInfo {
         string supplyAssetSymbol;
     }
 
-    function getKnownMorphoVaultsAddresses() internal pure returns (HashMap.Map memory) {
+    function getKnownMorphoVaultsAddresses()
+        internal
+        pure
+        returns (HashMap.Map memory)
+    {
         HashMap.Map memory knownVaults = HashMap.newMap();
         // === Mainnet morpho vaults ===
         // USDC (Gauntlet USDC Core)
@@ -368,60 +474,85 @@ library MorphoInfo {
         );
 
         // === Sepolia testnet morpho vaults ===
-        // None
+        // USDC (Legend USDC)
+        addMorphoVaultAddress(
+            knownVaults,
+            MorphoVaultKey({chainId: 11155111, supplyAssetSymbol: "USDC"}),
+            0x62559B2707013890FBB111280d2aE099a2EFc342
+        );
 
         // === Base Sepolia testnet morpho vaults ===
-        // None
+        // USDC (Legend USDC)
+        addMorphoVaultAddress(
+            knownVaults,
+            MorphoVaultKey({chainId: 84532, supplyAssetSymbol: "USDC"}),
+            0x6bcB6a167C190737858aE6122cc6804207D8eBF3
+        );
 
         return knownVaults;
     }
 
-    function getMorphoVaultAddress(uint256 chainId, string memory supplyAssetSymbol) internal pure returns (address) {
+    function getMorphoVaultAddress(
+        uint256 chainId,
+        string memory supplyAssetSymbol
+    ) internal pure returns (address) {
         HashMap.Map memory knownVaults = getKnownMorphoVaultsAddresses();
         return
-            getMorphoVaultAddress(knownVaults, MorphoVaultKey({chainId: chainId, supplyAssetSymbol: supplyAssetSymbol}));
+            getMorphoVaultAddress(
+                knownVaults,
+                MorphoVaultKey({
+                    chainId: chainId,
+                    supplyAssetSymbol: supplyAssetSymbol
+                })
+            );
     }
 
     // Helpers for map
-    function addMorphoVaultAddress(HashMap.Map memory knownVaults, MorphoVaultKey memory key, address addr)
-        internal
-        pure
-    {
+    function addMorphoVaultAddress(
+        HashMap.Map memory knownVaults,
+        MorphoVaultKey memory key,
+        address addr
+    ) internal pure {
         HashMap.put(knownVaults, abi.encode(key), abi.encode(addr));
     }
 
-    function getMorphoVaultAddress(HashMap.Map memory knownVaults, MorphoVaultKey memory key)
-        internal
-        pure
-        returns (address)
-    {
+    function getMorphoVaultAddress(
+        HashMap.Map memory knownVaults,
+        MorphoVaultKey memory key
+    ) internal pure returns (address) {
         if (!HashMap.contains(knownVaults, abi.encode(key))) {
             revert MorphoVaultNotFound();
         }
         return abi.decode(HashMap.get(knownVaults, abi.encode(key)), (address));
     }
 
-    function addMarketParams(HashMap.Map memory knownMarkets, MorphoMarketKey memory key, MarketParams memory params)
-        internal
-        pure
-    {
+    function addMarketParams(
+        HashMap.Map memory knownMarkets,
+        MorphoMarketKey memory key,
+        MarketParams memory params
+    ) internal pure {
         HashMap.put(knownMarkets, abi.encode(key), abi.encode(params));
     }
 
-    function getMarketParams(HashMap.Map memory knownMarkets, MorphoMarketKey memory key)
-        internal
-        pure
-        returns (MarketParams memory)
-    {
+    function getMarketParams(
+        HashMap.Map memory knownMarkets,
+        MorphoMarketKey memory key
+    ) internal pure returns (MarketParams memory) {
         if (!HashMap.contains(knownMarkets, abi.encode(key))) {
             revert MorphoMarketNotFound();
         }
-        return abi.decode(HashMap.get(knownMarkets, abi.encode(key)), (MarketParams));
+        return
+            abi.decode(
+                HashMap.get(knownMarkets, abi.encode(key)),
+                (MarketParams)
+            );
     }
 
     // Helper function to convert MarketParams to bytes32 Id
     // Reference: https://github.com/morpho-org/morpho-blue/blob/731e3f7ed97cf15f8fe00b86e4be5365eb3802ac/src/libraries/MarketParamsLib.sol
-    function marketId(MarketParams memory params) internal pure returns (bytes32) {
+    function marketId(
+        MarketParams memory params
+    ) internal pure returns (bytes32) {
         return keccak256(abi.encode(params));
     }
 }
