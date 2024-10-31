6211153d8a6c668247784681672fcd46ba0bdba1
scott-silver <silversh@gmail.com>
2024-10-31 14:40:51 -0700

    add Arbitrum (mainnet+sepolia) addresses (#97)

--- src/builder/BridgeRoutes.sol
+++ src/builder/BridgeRoutes.sol
@@ -26,16 +26,21 @@ library CCTP {
         address bridge;
     }
 
+    // @dev Source: TokenMessenger contract from https://developers.circle.com/stablecoins/evm-smart-contracts
     function knownChains() internal pure returns (CCTPChain[] memory) {
-        CCTPChain[] memory chains = new CCTPChain[](4);
+        CCTPChain[] memory chains = new CCTPChain[](6);
         // Mainnet
         chains[0] = CCTPChain({chainId: 1, domainId: 0, bridge: 0xBd3fa81B58Ba92a82136038B25aDec7066af3155});
         // Base
         chains[1] = CCTPChain({chainId: 8453, domainId: 6, bridge: 0x1682Ae6375C4E4A97e4B583BC394c861A46D8962});
+        // Arbitrum
+        chains[2] = CCTPChain({chainId: 42161, domainId: 3, bridge: 0x19330d10D9Cc8751218eaf51E8885D058642E08A});
         // Sepolia
-        chains[2] = CCTPChain({chainId: 11155111, domainId: 0, bridge: 0x9f3B8679c73C2Fef8b59B4f3444d4e156fb70AA5});
+        chains[3] = CCTPChain({chainId: 11155111, domainId: 0, bridge: 0x9f3B8679c73C2Fef8b59B4f3444d4e156fb70AA5});
         // Base Sepolia
-        chains[3] = CCTPChain({chainId: 84532, domainId: 6, bridge: 0x9f3B8679c73C2Fef8b59B4f3444d4e156fb70AA5});
+        chains[4] = CCTPChain({chainId: 84532, domainId: 6, bridge: 0x9f3B8679c73C2Fef8b59B4f3444d4e156fb70AA5});
+        // Arbitrum Sepolia
+        chains[5] = CCTPChain({chainId: 421614, domainId: 3, bridge: 0x9f3B8679c73C2Fef8b59B4f3444d4e156fb70AA5});
         return chains;
     }
 
--- src/builder/UniswapRouter.sol
+++ src/builder/UniswapRouter.sol
@@ -12,15 +12,19 @@ library UniswapRouter {
     /// @dev Addresses fetched from: https://docs.uniswap.org/contracts/v3/reference/deployments/
     /// Note: Make sure that these are the addresses for SwapRouter02, not SwapRouter.
     function knownChains() internal pure returns (RouterChain[] memory) {
-        RouterChain[] memory chains = new RouterChain[](4);
+        RouterChain[] memory chains = new RouterChain[](6);
         // Mainnet
         chains[0] = RouterChain({chainId: 1, router: 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45});
         // Base
         chains[1] = RouterChain({chainId: 8453, router: 0x2626664c2603336E57B271c5C0b26F421741e481});
+        // Arbitrum
+        chains[2] = RouterChain({chainId: 42161, router: 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45});
         // Sepolia
-        chains[2] = RouterChain({chainId: 11155111, router: 0x3bFA4769FB09eefC5a80d6E87c3B9C650f7Ae48E});
+        chains[3] = RouterChain({chainId: 11155111, router: 0x3bFA4769FB09eefC5a80d6E87c3B9C650f7Ae48E});
         // Base Sepolia
-        chains[3] = RouterChain({chainId: 84532, router: 0x94cC0AaC535CCDB3C01d6787D6413C739ae12bc4});
+        chains[4] = RouterChain({chainId: 84532, router: 0x94cC0AaC535CCDB3C01d6787D6413C739ae12bc4});
+        // Arbitrum Sepolia
+        chains[5] = RouterChain({chainId: 421614, router: 0x101F443B4d1b059569D643917553c771E1b9663E});
         return chains;
     }
 
